7b0cd7ebb8d9ead7c563bea752f3461efab8aabe
Johanbo22 <jbk@famkjaer.dk>
2025-12-31 02:12:48 +0100

    Refactor

---
+++ ui/SubplotOverlay.py
@@ -0,0 +1,67 @@
+from PyQt6.QtCore import QEasingCurve, QPropertyAnimation, Qt
+from PyQt6.QtGui import QColor, QPainter, QPen
+from PyQt6.QtWidgets import QGraphicsOpacityEffect, QLabel, QVBoxLayout, QWidget
+
+
+class SubplotOverlay(QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents)
+        self.setAttribute(Qt.WidgetAttribute.WA_NoSystemBackground)
+        self.hide()
+
+        self.v_layout = QVBoxLayout(self)
+        self.v_layout.setContentsMargins(0, 0, 0, 0)
+        self.v_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.label_widget = QLabel()
+        self.label_widget.setStyleSheet(
+            """
+            QLabel {
+                background-color: rgba(33, 150, 243, 0.8);
+                color: white;
+                font-weight: bold;
+                padding: 4px 8px;
+                border-radius: 4px;
+            }
+        """
+        )
+        self.v_layout.addWidget(self.label_widget)
+
+        self.opacity_effect = QGraphicsOpacityEffect(self)
+        self.setGraphicsEffect(self.opacity_effect)
+        self.fade_animation = QPropertyAnimation(self.opacity_effect, b"opacity")
+        self.fade_animation.setDuration(1500)
+        self.fade_animation.setStartValue(1.0)
+        self.fade_animation.setEndValue(0.2)
+        self.fade_animation.setEasingCurve(QEasingCurve.Type.InCubic)
+        self.fade_animation.finished.connect(self._on_animation_finished)
+
+    def _on_animation_finished(self):
+        """CAlled when the animation is finished to remove text but retain border values"""
+        self.label_widget.hide()
+
+    def update_info(self, text, geometry):
+        """Updater for text and geometry when information is changed"""
+        self.label_widget.setText(text)
+        self.label_widget.show()
+        self.setGeometry(*geometry)
+        self.show()
+
+        self.opacity_effect.setOpacity(1.0)
+        self.fade_animation.stop()
+        self.fade_animation.start()
+
+    def paintEvent(self, event):
+        """Draw a blue border around the subplot for the user to see"""
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        #the draw duude
+        pen = QPen(QColor("#2196F3"))
+        pen.setWidth(4)
+        painter.setPen(pen)
+
+        rect = self.rect().adjusted(2, 2, -2, -2)
+        painter.drawRect(rect)
--- ui/plot_tab.py
+++ ui/plot_tab.py
@@ -1,14 +1,14 @@
 # ui/plot_tab.py
 
 from re import M
-from PyQt6.QtWidgets import QMessageBox, QColorDialog, QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QComboBox, QSpinBox, QMessageBox, QFrame, QGraphicsOpacityEffect
-from PyQt6.QtCore import Qt, QTimer, QPropertyAnimation, QEasingCurve
-from PyQt6.QtGui import QIcon, QColor, QPalette, QPen, QPainter
-from attr import frozen
+from PyQt6.QtWidgets import QMessageBox, QColorDialog, QApplication, QHBoxLayout, QComboBox, QSpinBox, QMessageBox, QFrame
+from PyQt6.QtCore import QTimer
+from PyQt6.QtGui import QIcon, QColor, QPalette
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt import NavigationToolbar2QT as NavigationToolbar
 from core.plot_engine import PlotEngine
 from core.data_handler import DataHandler
+from ui.SubplotOverlay import SubplotOverlay
 from ui.status_bar import StatusBar
 from core.code_exporter import CodeExporter
 from ui.dialogs import ProgressDialog, ScriptEditorDialog
@@ -25,69 +25,6 @@ import traceback
 from matplotlib.colors import to_hex
 from typing import Dict, List, Any
 
-
-class SubplotOverlay(QWidget):
-    
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents)
-        self.setAttribute(Qt.WidgetAttribute.WA_NoSystemBackground)
-        self.hide()
-
-        self.v_layout = QVBoxLayout(self)
-        self.v_layout.setContentsMargins(0, 0, 0, 0)
-        self.v_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.label_widget = QLabel()
-        self.label_widget.setStyleSheet(
-            """
-            QLabel {
-                background-color: rgba(33, 150, 243, 0.8);
-                color: white;
-                font-weight: bold;
-                padding: 4px 8px;
-                border-radius: 4px;
-            }
-        """
-        )
-        self.v_layout.addWidget(self.label_widget)
-
-        self.opacity_effect = QGraphicsOpacityEffect(self)
-        self.setGraphicsEffect(self.opacity_effect)
-        self.fade_animation = QPropertyAnimation(self.opacity_effect, b"opacity")
-        self.fade_animation.setDuration(1500)
-        self.fade_animation.setStartValue(1.0)
-        self.fade_animation.setEndValue(0.2)
-        self.fade_animation.setEasingCurve(QEasingCurve.Type.InCubic)
-        self.fade_animation.finished.connect(self._on_animation_finished)
-    
-    def _on_animation_finished(self):
-        """CAlled when the animation is finished to remove text but retain border values"""
-        self.label_widget.hide()
-
-    def update_info(self, text, geometry):
-        """Updater for text and geometry when information is changed"""
-        self.label_widget.setText(text)
-        self.label_widget.show()
-        self.setGeometry(*geometry)
-        self.show()
-
-        self.opacity_effect.setOpacity(1.0)
-        self.fade_animation.stop()
-        self.fade_animation.start()
-    
-    def paintEvent(self, event):
-        """Draw a blue border around the subplot for the user to see"""
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
-
-        #the draw duude
-        pen = QPen(QColor("#2196F3"))
-        pen.setWidth(4)
-        painter.setPen(pen)
-        
-        rect = self.rect().adjusted(2, 2, -2, -2)
-        painter.drawRect(rect)
 
 class PlotTab(PlotTabUI):
     """Tab for creating and customizing plots"""
