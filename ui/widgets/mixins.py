9ebf48a7c241b39e3ba5c4d71a9d1f703d6732b0
Johanbo22 <jbk@famkjaer.dk>
2026-02-24 14:29:44 +0100

    Better styling for the spinbox widget, also changed issues with inheritance through the widget mixer

--- ui/widgets/mixins.py
+++ ui/widgets/mixins.py
@@ -11,7 +11,8 @@ class HoverFocusAnimationMixin:
     - Implement a _update_stylesheet(self, color: QColor) method.
     """
     
-    def __init__(self, base_color: QColor = None, hover_color: QColor = None, focus_color: QColor = None):
+    def __init__(self, base_color: QColor = None, hover_color: QColor = None, focus_color: QColor = None, **kwargs):
+        super().__init__(**kwargs)
         # Allow for custom colors,
         # but default to ThemeColors
         self._base_border_color = base_color if base_color else ThemeColors.BORDER_BASE
