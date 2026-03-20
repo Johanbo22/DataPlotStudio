5818461b7ec450d01a55af92056ef22a666412ec
Johanbo22 <jbk@famkjaer.dk>
2026-03-20 18:14:15 +0100

    updates and fixes for SubplotOverlay: - rendering bugs of the edges for high DPI - fixed text not disappearing when animation finished

--- ui/SubplotOverlay.py
+++ ui/SubplotOverlay.py
@@ -1,12 +1,13 @@
-from PyQt6.QtCore import QEasingCurve, QPropertyAnimation, Qt
-from PyQt6.QtGui import QColor, QPainter, QPen
+from PyQt6.QtCore import QEasingCurve, QPropertyAnimation, Qt, pyqtProperty, QSequentialAnimationGroup, QPauseAnimation, QRectF
+from PyQt6.QtGui import QColor, QPainter, QPen, QPaintEvent
 from PyQt6.QtWidgets import QGraphicsOpacityEffect, QLabel, QVBoxLayout, QWidget
 
 
 class SubplotOverlay(QWidget):
 
-    def __init__(self, parent=None):
+    def __init__(self, parent: QWidget | None = None) -> None:
         super().__init__(parent)
+        self._border_color: QColor = QColor("#2196F3")
         self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents)
         self.setAttribute(Qt.WidgetAttribute.WA_NoSystemBackground)
         self.hide()
@@ -26,18 +27,37 @@ class SubplotOverlay(QWidget):
 
         self.opacity_effect = QGraphicsOpacityEffect(self)
         self.setGraphicsEffect(self.opacity_effect)
+        
         self.fade_animation = QPropertyAnimation(self.opacity_effect, b"opacity")
-        self.fade_animation.setDuration(1500)
+        self.fade_animation.setDuration(1000)
         self.fade_animation.setStartValue(1.0)
-        self.fade_animation.setEndValue(0.2)
+        self.fade_animation.setEndValue(0.1)
         self.fade_animation.setEasingCurve(QEasingCurve.Type.InCubic)
+        
+        self.animation_sequence = QSequentialAnimationGroup(self)
+        
+        self.pause_animation = QPauseAnimation(1000, self)
+        self.animation_sequence.addAnimation(self.pause_animation)
+        self.animation_sequence.addAnimation(self.fade_animation)
+        
         self.fade_animation.finished.connect(self._on_animation_finished)
+        
+    @pyqtProperty(QColor)
+    def border_color(self) -> QColor:
+        return self._border_color
+    
+    @border_color.setter
+    def border_color(self, color: QColor) -> None:
+        self._border_color = color
+        self.update()
     
     def show_update_required(self, visible: bool = True) -> None:
         self.update_notice_label.setVisible(visible)
         if visible:
+            self.animation_sequence.stop()
+            self.opacity_effect.setOpacity(1.0)
             self.show()
-        elif not self.label_widget.text():
+        elif not self.label_widget.text() and self.opacity_effect.opacity() == 1.0:
             self.hide()
 
     def _on_animation_finished(self):
@@ -59,18 +79,21 @@ class SubplotOverlay(QWidget):
             self.show()
 
             self.opacity_effect.setOpacity(1.0)
-            self.fade_animation.stop()
-            self.fade_animation.start()
+            self.animation_sequence.stop()
+            self.animation_sequence.start()
 
-    def paintEvent(self, event):
+    def paintEvent(self, event: QPaintEvent):
         """Draw a blue border around the subplot for the user to see"""
         painter = QPainter(self)
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
 
         #the draw duude
-        pen = QPen(QColor("#2196F3"))
+        pen = QPen(self._border_color)
         pen.setWidth(4)
+        
+        pen.setCapStyle(Qt.PenCapStyle.SquareCap)
+        pen.setJoinStyle(Qt.PenJoinStyle.MiterJoin)
         painter.setPen(pen)
 
-        rect = self.rect().adjusted(2, 2, -2, -2)
+        rect = QRectF(self.rect()).adjusted(2.0, 2.0, -2.0, -2.0)
         painter.drawRect(rect)
